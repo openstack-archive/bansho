a638ec445e33242931382c3f04cf5f85736aaebd
Frédéric Vachon <fredvac@gmail.com>
2015-01-16 16:35:27 -0500

    Update unit tests

--- app/navbar/navbar_test.js
+++ app/navbar/navbar_test.js
@@ -1,18 +1,43 @@
 'use strict';
 
 describe('Navbar module', function () {
+    var $compile,
+        $rootScope,
+        $controller,
+        $httpBackend;
 
     beforeEach(module('adagios.navbar'));
 
+    beforeEach(inject(function (_$compile_, _$rootScope_, _$controller_, _$httpBackend_) {
+        $compile = _$compile_;
+        $rootScope = _$rootScope_;
+        $controller = _$controller_;
+        $httpBackend = _$httpBackend_;
+
+        $httpBackend.expectGET('navbar/navbar.html').respond('<a>{{ notifications }}</a>');
+    }));
+
     describe('NavBarCtrl', function () {
 
-        it('should be defined', inject(function ($controller) {
-            var scope, ctrl;
-            scope = {};
-            ctrl = $controller('NavBarCtrl', { $scope : scope });
+        it('should be defined', function () {
+            var scope = $rootScope.$new(),
+                navbar = $controller('NavBarCtrl', { $scope : scope });
 
-            expect(ctrl).toBeDefined();
-        }));
+            expect(navbar).toBeDefined();
+        });
+
+    });
+
+    describe('Navbar directive', function () {
+
+        it('should insert the number of warnings', function () {
+            var element = $compile('<navbar></navbar>')($rootScope);
+            $httpBackend.flush();
+            $rootScope.notifications = 44;
+            $rootScope.$digest();
+
+            expect(element.text()).toBe('44');
+        });
 
     });
 });
