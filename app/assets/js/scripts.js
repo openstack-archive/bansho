fed09010cf635ed803853662c77a534d78240bc0
Hugo Soucy <hugo.soucy@savoirfairelinux.com>
2015-01-22 14:39:36 -0500

    [#64040] Effacer du code commenter

--- app/assets/js/scripts.js
+++ app/assets/js/scripts.js
@@ -23,22 +23,3 @@ var SFLfront = (function() {
 		mquery: mquery
 	};
 })();
-
-//     //var mqDesktop = 'only screen and (min-width: 60em)';
-//     //var mqTablet = 'only screen and (min-width: 30.063em) and (max-width: 59.938em)';
-//     //var mqPhone = 'only screen and (min-width: 20em) and (max-width: 30em)';
-
-//     $(function () {
-//         // ON LOAD
-//         console.log($('li'), '!!!!');
-
-//         $('a').on('click', function(e) {
-//             e.preventDefault();
-//             console.log($('!!!'));
-//         });
-
-//         // ON RESIZE
-//     });
-
-
-// })(jQuery, window, document);
