e52e12da546b3acb51e87e377a3c03103e517b09
Hugo Soucy <hugo.soucy@savoirfairelinux.com>
2015-02-12 10:23:19 -0500

    Travail sur le tableau principal (HTML/CSS).

--- app/components/tactical/tactical.js
+++ app/components/tactical/tactical.js
@@ -4,8 +4,13 @@ angular.module('adagios.tactical', ['adagios.tactical.status_overview',
                                     'adagios.tactical.current_health',
                                     'adagios.tactical.top_alert_producers',
                                     'adagios.table'
-                                    ])
+                                   ])
 
     .controller('TacticalCtrl', ['$scope', '$http', function ($scope, $http) {
-        return;
+
+        // Togglable tabs
+        // Don't follow hyperlinks
+        $('a[data-toggle="tab"]').on('click', function(evt) {
+            evt.preventDefault();
+        });
     }]);
