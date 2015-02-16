a254b8ab6cc38cc0ce03c0193a67d219317a00aa
Frédéric Vachon <fredvac@gmail.com>
2015-02-16 14:31:49 -0500

    Enable table header name support in services and hosts views

--- app/hosts/hosts.js
+++ app/hosts/hosts.js
@@ -14,13 +14,15 @@ angular.module('adagios.view.hosts', ['ngRoute',
     }])
 
     .controller('HostsCtrl', ['$scope', 'hostsConfig', function ($scope, hostsConfig) {
-        $scope.hostsCells = hostsConfig.cells.join();
+        $scope.hostsCellsText = hostsConfig.cellsText.join();
+        $scope.hostsCellsName = hostsConfig.cellsName.join();
         $scope.hostsApiName = hostsConfig.apiName;
         $scope.hostsFilters = hostsConfig.filters;
     }])
 
     .run(['readConfig', 'hostsConfig', function (readConfig, hostsConfig) {
-        hostsConfig.cells = readConfig.data.hostsConfig.cells;
+        hostsConfig.cellsText = readConfig.data.hostsConfig.cells.text;
+        hostsConfig.cellsName = readConfig.data.hostsConfig.cells.name;
         hostsConfig.apiName = readConfig.data.hostsConfig.apiName;
         hostsConfig.filters = readConfig.data.hostsConfig.filters;
     }]);
--- app/services/services.js
+++ app/services/services.js
@@ -14,13 +14,15 @@ angular.module('adagios.view.services', ['ngRoute',
     }])
 
     .controller('ServicesCtrl', ['$scope', 'servicesConfig', function ($scope, servicesConfig) {
-        $scope.servicesCells = servicesConfig.cells.join();
+        $scope.servicesCellsText = servicesConfig.cellsText.join();
+        $scope.servicesCellsName = servicesConfig.cellsName.join();
         $scope.servicesApiName = servicesConfig.apiName;
         $scope.servicesFilters = servicesConfig.filters;
     }])
 
     .run(['readConfig', 'servicesConfig', function (readConfig, servicesConfig) {
-        servicesConfig.cells = readConfig.data.servicesConfig.cells;
+        servicesConfig.cellsText = readConfig.data.servicesConfig.cells.text;
+        servicesConfig.cellsName = readConfig.data.servicesConfig.cells.name;
         servicesConfig.apiName = readConfig.data.servicesConfig.apiName;
         servicesConfig.filters = readConfig.data.servicesConfig.filters;
     }]);
