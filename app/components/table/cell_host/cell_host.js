3f8c39853fd17868e052df902957c3cb6a2fd8dd
Frédéric Vachon <fredvac@gmail.com>
2015-02-16 10:10:38 -0500

    Extract static table to table and cell modules

--- app/components/table/cell_host/cell_host.js
+++ app/components/table/cell_host/cell_host.js
@@ -3,7 +3,13 @@
 angular.module('adagios.table.cell_host', ['adagios.table'])
 
     .controller('CellHostCtrl', ['$scope', function ($scope) {
-        angular.noop();
+        if ($scope.entry.host_state == 0) {
+            $scope.state = 'state--ok';
+        } else if ($scope.entry.host_state == 1) {
+            $scope.state = 'state--warning';
+        } else {
+            $scope.state = 'state--error';
+        }
     }])
 
     .run(['tableConfig', function (tableConfig) {
