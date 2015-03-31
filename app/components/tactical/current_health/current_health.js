faf36eebdbdaee7cb3f08e9f0bcb618d01db8b45
Frédéric Vachon <fredvac@gmail.com>
2015-03-31 15:36:16 -0400

    Status overview displays real data

--- app/components/tactical/current_health/current_health.js
+++ app/components/tactical/current_health/current_health.js
@@ -3,27 +3,9 @@
 angular.module('adagios.tactical.current_health', ['adagios.live',
                                                    'ngJustGage'])
 
-    .controller('TacticalCurrentHealth', ['$scope', 'getHostProblems', 'getServiceProblems', 'getTotalHosts', 'getTotalServices',
-        function ($scope, getHostProblems, getServiceProblems, getTotalHosts, getTotalServices) {
-            $scope.hostsRatio = 0;
-            $scope.servicesRatio = 0;
-
-            getHostProblems.success(function (data) {
-                $scope.hostProblems = data.length;
-                getTotalHosts.success(function (data) {
-                    $scope.totalHosts = data.length;
-                    $scope.hostsRatio = ($scope.totalHosts - $scope.hostProblems) / $scope.totalHosts * 100;
-                });
-            });
-
-            getServiceProblems.success(function (data) {
-                $scope.serviceProblems = data.length;
-                getTotalServices.success(function (data) {
-                    $scope.totalServices = data.length;
-                    $scope.servicesRatio = ($scope.totalServices - $scope.serviceProblems) / $scope.totalServices * 100;
-                });
-            });
-        }])
+    .controller('TacticalCurrentHealth', ['$scope', function ($scope) {
+        angular.noop();
+    }])
 
     .directive('adgCurrentHealth', function () {
         return {
--- app/components/tactical/tactical.js
+++ app/components/tactical/tactical.js
@@ -14,17 +14,41 @@ angular.module('adagios.tactical', ['adagios.tactical.status_overview',
         this.topAlertProducers = config.components.topAlertProducers;
     })
 
-    .controller('TacticalCtrl', ['$scope', 'tacticalConfig', function ($scope, tacticalConfig) {
+    .controller('TacticalCtrl', ['$scope', 'tacticalConfig', 'getHostProblems', 'getServiceProblems',
+        'getTotalHosts', 'getTotalServices',
+        function ($scope, tacticalConfig, getHostProblems, getServiceProblems, getTotalHosts, getTotalServices) {
+            $scope.statusOverview = tacticalConfig.statusOverview;
+            $scope.currentHealth = tacticalConfig.currentHealth;
+            $scope.topAlertProducers = tacticalConfig.topAlertProducers;
 
-        $scope.statusOverview = tacticalConfig.statusOverview;
-        $scope.currentHealth = tacticalConfig.currentHealth;
-        $scope.topAlertProducers = tacticalConfig.topAlertProducers;
+            $scope.hostsRatio = 0;
+            $scope.servicesRatio = 0;
+            $scope.hostProblems = 0;
+            $scope.totalHosts = 0;
+            $scope.serviceProblems = 0;
+            $scope.totalServices = 0;
 
-        // Togglable tabs
-        // Don't follow hyperlinks
-        $('a[data-toggle="tab"]').on('click', function (evt) {
-            evt.preventDefault();
-        });
+            getHostProblems.success(function (data) {
+                $scope.hostProblems = data.length;
+                getTotalHosts.success(function (data) {
+                    $scope.totalHosts = data.length;
+                    $scope.hostsRatio = ($scope.totalHosts - $scope.hostProblems) / $scope.totalHosts * 100;
+                });
+            });
+
+            getServiceProblems.success(function (data) {
+                $scope.serviceProblems = data.length;
+                getTotalServices.success(function (data) {
+                    $scope.totalServices = data.length;
+                    $scope.servicesRatio = ($scope.totalServices - $scope.serviceProblems) / $scope.totalServices * 100;
+                });
+            });
+
+            // Togglable tabs
+            // Don't follow hyperlinks
+            $('a[data-toggle="tab"]').on('click', function (evt) {
+                evt.preventDefault();
+            });
     }])
 
     .directive('adgTactical', ['tacticalConfig', function (tacticalConfig) {
