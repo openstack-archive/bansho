dba9d119f8e0f6fc4cb5d096712c4b8889a13501
aviau <alexandre.viau@savoirfairelinux.com>
2015-05-06 17:20:18 -0400

    Added BANSHO_AUTH_URL variable
    Change-Id: I809ab56f34673c6737033b3ea72db05f4fac96d1

--- container/configure.sh
+++ container/configure.sh
@@ -5,9 +5,13 @@ CONFIG_FILE="/etc/apache2/sites-enabled/000-default.conf"
 echo "=> Configuring apache ProxyPass ..."
 
 # Surveil URLS
-sed -i "s|ProxyPass /surveil/.*|ProxyPass /surveil/ ${BANSHO_SURVEIL_URL}|g" ${CONFIG_FILE}
-sed -i "s|ProxyPassReverse /surveil/.*|ProxyPassReverse /surveil/ ${BANSHO_SURVEIL_URL}|g" ${CONFIG_FILE}
+sed -i "s|ProxyPass /surveil/ .*|ProxyPass /surveil/ ${BANSHO_SURVEIL_URL}|g" ${CONFIG_FILE}
+sed -i "s|ProxyPassReverse /surveil/ .*|ProxyPassReverse /surveil/ ${BANSHO_SURVEIL_URL}|g" ${CONFIG_FILE}
 
 # Adagios URLS
 sed -i "s|ProxyPass /adagios/.*|ProxyPass /adagios/ ${BANSHO_ADAGIOS_URL}|g" ${CONFIG_FILE}
 sed -i "s|ProxyPassReverse /adagios/.*|ProxyPassReverse /adagios/ ${BANSHO_ADAGIOS_URL}|g" ${CONFIG_FILE}
+
+# Bansho auth URL
+sed -i "s|ProxyPass /surveil/v2/auth/.*|ProxyPass /surveil/v2/auth/ ${BANSHO_AUTH_URL}|g" ${CONFIG_FILE}
+sed -i "s|ProxyPassReverse /surveil/v2/auth/.*|ProxyPass /surveil/v2/auth/ ${BANSHO_AUTH_URL}|g" ${CONFIG_FILE}
