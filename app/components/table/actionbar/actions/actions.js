d8b035be95f4bd37bb0b9b730f1f256a7222b80a
Alexandre Viau <alexandre@alexandreviau.net>
2015-05-15 14:02:02 -0400

    Improve login and logout + replaced tabs by spaces
    Change-Id: I15ea3a23024a8c992b56429a5352746c9aa598bc

--- app/components/table/actionbar/actions/actions.js
+++ app/components/table/actionbar/actions/actions.js
@@ -2,134 +2,134 @@
 
 angular.module('bansho.table.actionbar')
 
-	.directive('banshoAcknowledgeForm', function () {
-		return {
-			restrict: 'E',
-			templateUrl: 'components/table/actionbar/actions/acknowledge_form.html',
-			scope: {
-				isShown: '='
-			},
-			controller: 'banshoAcknowledgeFormCtrl'
-		};
-	})
+    .directive('banshoAcknowledgeForm', function () {
+        return {
+            restrict: 'E',
+            templateUrl: 'components/table/actionbar/actions/acknowledge_form.html',
+            scope: {
+                isShown: '='
+            },
+            controller: 'banshoAcknowledgeFormCtrl'
+        };
+    })
 
-	.controller('banshoAcknowledgeFormCtrl',
-		['$scope', '$filter', 'tablesConfig', 'actionbarFilters', 'backendClient',
-		function ($scope, $filter, tablesConfig, actionbarFilters, backendClient) {
+    .controller('banshoAcknowledgeFormCtrl',
+        ['$scope', '$filter', 'tablesConfig', 'actionbarFilters', 'backendClient',
+        function ($scope, $filter, tablesConfig, actionbarFilters, backendClient) {
 
-		$scope.acknowledgeProblems = function () {
-			angular.forEach(tablesConfig, function (tableConfig) {
-				var entries = $filter('filter')(tableConfig.entries,
-					actionbarFilters.searchFilter);
+        $scope.acknowledgeProblems = function () {
+            angular.forEach(tablesConfig, function (tableConfig) {
+                var entries = $filter('filter')(tableConfig.entries,
+                    actionbarFilters.searchFilter);
 
-				angular.forEach(entries, function (entry) {
-					var service_description;
+                angular.forEach(entries, function (entry) {
+                    var service_description;
 
-					if (entry.is_checked) {
-						if ('description' in entry) {
-							service_description = entry.description;
-						}
+                    if (entry.is_checked) {
+                        if ('description' in entry) {
+                            service_description = entry.description;
+                        }
 
-						backendClient.acknowledge(entry.host_name, service_description, $scope.attrs).error(function (data) {
-								throw new Error('Acknowledge request failed');
-							});
-					}
-				});
-			});
-		};
-	}])
+                        backendClient.acknowledge(entry.host_name, service_description, $scope.attrs).error(function (data) {
+                                throw new Error('Acknowledge request failed');
+                            });
+                    }
+                });
+            });
+        };
+    }])
 
-	.directive('banshoDowntimeForm', function () {
-		return {
-			restrict: 'E',
-			templateUrl: 'components/table/actionbar/actions/downtime_form.html',
-			scope: {
-				isShown: '='
-			},
-			controller: 'banshoDowntimeFormCtrl'
-		};
-	})
+    .directive('banshoDowntimeForm', function () {
+        return {
+            restrict: 'E',
+            templateUrl: 'components/table/actionbar/actions/downtime_form.html',
+            scope: {
+                isShown: '='
+            },
+            controller: 'banshoDowntimeFormCtrl'
+        };
+    })
 
-	.controller('banshoDowntimeFormCtrl',
-		['$scope', '$filter', 'tablesConfig', 'actionbarFilters', 'backendClient',
-		function ($scope, $filter, tablesConfig, actionbarFilters, backendClient) {
+    .controller('banshoDowntimeFormCtrl',
+        ['$scope', '$filter', 'tablesConfig', 'actionbarFilters', 'backendClient',
+        function ($scope, $filter, tablesConfig, actionbarFilters, backendClient) {
 
-		$scope.messages = [];
+        $scope.messages = [];
 
-		$scope.sendDowntime = function () {
-			angular.forEach(tablesConfig, function (table) {
-				var entries = $filter('filter')(table.entries, actionbarFilters.searchFilter);
+        $scope.sendDowntime = function () {
+            angular.forEach(tablesConfig, function (table) {
+                var entries = $filter('filter')(table.entries, actionbarFilters.searchFilter);
 
-				angular.forEach(entries, function (entry) {
-					var service_description;
+                angular.forEach(entries, function (entry) {
+                    var service_description;
 
-					if (entry.is_checked) {
-						if ('description' in entry) {
-							service_description = entry.description;
-						}
+                    if (entry.is_checked) {
+                        if ('description' in entry) {
+                            service_description = entry.description;
+                        }
 
-						backendClient.downtime(entry.host_name, service_description, $scope.attrs).then(function (data) {
-							$scope.messages.push({
-								text: entry.host_name + " success ",
-								type: "success"
-							});
-							$scope.isShown = false;
-						},
-						function (error) {
-							$scope.messages.push({
-								text: entry.host_name + " error",
-								type: "error"
-							});
-						});
-					}
-				});
-			});
-		};
-	}])
+                        backendClient.downtime(entry.host_name, service_description, $scope.attrs).then(function (data) {
+                            $scope.messages.push({
+                                text: entry.host_name + " success ",
+                                type: "success"
+                            });
+                            $scope.isShown = false;
+                        },
+                        function (error) {
+                            $scope.messages.push({
+                                text: entry.host_name + " error",
+                                type: "error"
+                            });
+                        });
+                    }
+                });
+            });
+        };
+    }])
 
-	.directive('banshoRecheckButton', function () {
-		return {
-			restrict: 'E',
-			templateUrl: 'components/table/actionbar/actions/recheck_button.html',
-			scope: {
-				isShown: '='
-			},
-			controller: 'banshoRecheckButtonCtrl'
-		};
-	})
+    .directive('banshoRecheckButton', function () {
+        return {
+            restrict: 'E',
+            templateUrl: 'components/table/actionbar/actions/recheck_button.html',
+            scope: {
+                isShown: '='
+            },
+            controller: 'banshoRecheckButtonCtrl'
+        };
+    })
 
-	.controller('banshoRecheckButtonCtrl',
-		['$scope', '$filter', 'tablesConfig', 'actionbarFilters', 'backendClient',
-		function ($scope, $filter, tablesConfig, actionbarFilters, backendClient) {
+    .controller('banshoRecheckButtonCtrl',
+        ['$scope', '$filter', 'tablesConfig', 'actionbarFilters', 'backendClient',
+        function ($scope, $filter, tablesConfig, actionbarFilters, backendClient) {
 
-		$scope.messages = [];
+        $scope.messages = [];
 
-		$scope.sendRecheck = function () {
-			angular.forEach(tablesConfig, function (table) {
-				var entries = $filter('filter')(table.entries, actionbarFilters.searchFilter);
+        $scope.sendRecheck = function () {
+            angular.forEach(tablesConfig, function (table) {
+                var entries = $filter('filter')(table.entries, actionbarFilters.searchFilter);
 
-				angular.forEach(entries, function (entry) {
-					var service_description;
+                angular.forEach(entries, function (entry) {
+                    var service_description;
 
-					if (entry.is_checked) {
-						if ('description' in entry) {
-							service_description = entry.description;
-						}
+                    if (entry.is_checked) {
+                        if ('description' in entry) {
+                            service_description = entry.description;
+                        }
 
-						backendClient.recheck(entry.host_name, service_description).then(function (data) {
-							$scope.messages.push({
-								text: entry.host_name + " success ",
-								type: "success"
-							});
-						},
-						function (error) {
-							$scope.messages.push({
-								text: entry.host_name + " error",
-								type: "error"
-							});
-						});
-					}
-				});
-			});
-		};
-	}]);
+                        backendClient.recheck(entry.host_name, service_description).then(function (data) {
+                            $scope.messages.push({
+                                text: entry.host_name + " success ",
+                                type: "success"
+                            });
+                        },
+                        function (error) {
+                            $scope.messages.push({
+                                text: entry.host_name + " error",
+                                type: "error"
+                            });
+                        });
+                    }
+                });
+            });
+        };
+    }]);
