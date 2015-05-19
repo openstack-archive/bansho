f07b6a511de061288c5852d877d3bbbbddd5c0fa
aviau <alexandre.viau@savoirfairelinux.com>
2015-05-19 15:27:28 -0400

    Actionbar: Added notifications
    Change-Id: Ib6889e9097604dd05412dab07b3d12bd97560d96

---
+++ app/components/notifications/notifications.js
@@ -0,0 +1,23 @@
+'use strict';
+
+angular.module('bansho.notifications', [])
+    .service('notifications', [
+        function () {
+
+            var push = function (type, title, message) {
+                $(function(){
+                    console.log(PNotify);
+                    new PNotify({
+                        type: type,
+                        title: title,
+                        text: message,
+                        styling: {},
+                    });
+                });
+            };
+
+            return {
+                "push": push
+            }
+
+        }]);
