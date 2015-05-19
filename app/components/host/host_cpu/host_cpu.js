230ee35a5efe4e39437bcc072fcdb809d277d254
Vincent Fournier <v.fournier11@gmail.com>
2015-05-19 14:47:48 -0400

    Improve service page and directives
    Change-Id: Ie400740374f0222f20c00610c78a4e5d40c6004f

--- app/components/host/host_cpu/host_cpu.js
+++ app/components/host/host_cpu/host_cpu.js
@@ -1,24 +1,12 @@
 'use strict';
 
 angular.module('bansho.host.cpu', ['bansho.live'])
-
-    .controller('HostCpuCtrl', ['$scope', 'backendClient', function ($scope, backendClient) {
-        var hostName = $scope.hostName,
-            service = 'cpu',
-            fields = ['state', 'description', 'plugin_output'],
-            filters = {},
-            apiName = 'services',
-            additionnalFields = {'host_name': hostName, 'description': service};
-
-        backendClient.getObjects(fields, filters, apiName, additionnalFields)
-            .success(function (data) {
-                $scope.cpuData = data[0];
-            });
-    }])
-
     .directive('banshoHostCpu', function () {
         return {
             restrict: 'E',
+            compile: function (scope, element, attrs) {
+                scope.host = attrs.host;
+            },
             templateUrl: 'components/host/host_cpu/host_cpu.html'
         };
     });
