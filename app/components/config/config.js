1b87be8ab0e8a2eb129a2e7d25d28cbbe00f38fb
aviau <alexandre.viau@savoirfairelinux.com>
2015-05-20 10:57:48 -0400

    developmentConfig: New option to ignore server config
    Change-Id: If8357cc815abd53fd0667bf9539b4df3259f8434

--- app/components/config/config.js
+++ app/components/config/config.js
@@ -40,12 +40,12 @@ angular.module('bansho.config', [])
             return config.data;
         };
 
-        this.fetchConfig = function () {
+        this.fetchConfig = function (useServerConfig) {
             var responsePromise = $q.defer();
 
             $http.get('surveil/v2/bansho/config')
                 .success(function (conf) {
-                    if (jQuery.isEmptyObject(conf))  {
+                    if (!useServerConfig || jQuery.isEmptyObject(conf))  {
 
                         $http.get('components/config/config.json')
                             .success(function (conf) {
