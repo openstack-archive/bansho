eb36bd3bfe43e97314109a53ea1fb5295398a9a8
flavien.peyre <peyre.flavien@gmail.com>
2015-05-20 09:22:44 -0400

    Improve service page
    Change-Id: Icca92862209442cd74177113ba2348e1159c7677

--- app/components/service/service_info/service_info.js
+++ app/components/service/service_info/service_info.js
@@ -3,12 +3,15 @@
 angular.module('bansho.service.info', [])
 
     .controller('ServiceInfoCtrl', ['$scope', function ($scope) {
-        $scope.acknowledged = $scope.data[0].acknowledged === "1" ? "Yes" : "No";
+        $scope.isAcknowledged = $scope.acknowledged === "1" ? "Yes" : "No";
     }])
 
     .directive('banshoServiceInfo', function () {
         return {
             restrict: 'E',
+            scope: {
+              service: '=service'
+            },
             templateUrl: 'components/service/service_info/service_info.html'
         };
     });
