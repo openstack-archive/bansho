ac24ea54073c73b2c31ecdb1ef726f55033e572e
Vincent Fournier <v.fournier11@gmail.com>
2015-06-01 22:38:21 -0400

    Add Grafana configuration to docker file and add graph
    Change-Id: I687c1c24fed49a98f83015cab73879a3323432fb

---
+++ app/components/grafana/grafana.js
@@ -0,0 +1,23 @@
+'use strict';
+
+angular.module('bansho.grafana', [])
+    .service('iframeUrl', function () {
+        var createIFrameUrl = function (templateName, host_name, service_description, metric) {
+            return '/grafana/dashboard-solo/db/' + templateName + '?' +
+                    'panelId=' + '1' +
+                    '&fullscreen&from=' + 'now-6h' +
+                    '&to=' + 'now' +
+                    '&var-metric1=' + metric +
+                    '&var-host_name=' + host_name +
+                    '&var-service_description=' + service_description;
+        };
+
+        var getIFrameUrl = function (metric, host_name, service_description) {
+            return createIFrameUrl('templatehostnameservicedescriptiononemetric',
+                                    host_name, service_description, metric);
+        };
+
+        return {
+            getIFrameUrl: getIFrameUrl
+        };
+    });
