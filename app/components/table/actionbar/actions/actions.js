de92da6c282f440818615a5e5bad78a747e9a81f
Vincent Fournier <v.fournier11@gmail.com>
2015-07-13 17:37:04 -0400

    Refactoring of table
    Change-Id: Ib7dff08eac97d3fe1a9412c50a9538dfdd4f9a54

--- app/components/table/actionbar/actions/actions.js
+++ app/components/table/actionbar/actions/actions.js
@@ -9,41 +9,34 @@ angular.module('bansho.table.actionbar')
             scope: {
                 isShown: '='
             },
-            controller: 'banshoAcknowledgeFormCtrl'
+            controller: ['$scope', 'tables', 'surveilActions', 'notifications',
+                function ($scope, tables, surveilActions, notifications) {
+                    $scope.acknowledgeProblems = function () {
+                        angular.forEach($scope.tableId, function (tableId) {
+                            tables.forEachCheckedEntries(tableId, function (entry) {
+                                var service_description;
+
+                                if ('description' in entry) {
+                                    service_description = entry.description;
+                                }
+
+                                surveilActions.acknowledge(entry.host_name, service_description, $scope.attrs).then(function (data) {
+                                        notifications.push('success', 'Acknowledgement', 'Acknowledged ' + entry.host_name);
+                                    },
+                                    function (error) {
+                                        notifications.push('error', 'Acknowledgement', 'Could not acknowledge ' + entry.host_name);
+                                    });
+                            });
+
+                            tables.setAllCheckTable(tableId, false);
+                        });
+
+                        $scope.isShown = false;
+                    };
+                }
+            ]
         };
     })
-
-    .controller('banshoAcknowledgeFormCtrl',
-        ['$scope', '$filter', 'tablesConfig', 'actionbarFilters', 'surveilActions', 'notifications',
-        function ($scope, $filter, tablesConfig, actionbarFilters, surveilActions, notifications) {
-
-        $scope.acknowledgeProblems = function () {
-            angular.forEach(tablesConfig, function (table) {
-                var entries = $filter('filter')(table.entries,
-                    actionbarFilters.searchFilter);
-                table.isCheckAll = false;
-
-                angular.forEach(entries, function (entry) {
-                    var service_description;
-
-                    if (entry.is_checked) {
-                        entry.is_checked = false;
-                        if ('description' in entry) {
-                            service_description = entry.description;
-                        }
-
-                        surveilActions.acknowledge(entry.host_name, service_description, $scope.attrs).then(function (data) {
-                            notifications.push('success', 'Acknowledgement', 'Acknowledged ' + entry.host_name);
-                        },
-                        function (error) {
-                            notifications.push('error', 'Acknowledgement', 'Could not acknowledge ' + entry.host_name);
-                        });
-                    }
-                });
-            });
-            $scope.isShown = false;
-        };
-    }])
 
     .directive('banshoDowntimeForm', function () {
         return {
@@ -52,75 +45,62 @@ angular.module('bansho.table.actionbar')
             scope: {
                 isShown: '='
             },
-            controller: 'banshoDowntimeFormCtrl'
+            controller: ['$scope', 'tables', 'surveilActions', 'notifications',
+                function ($scope, tables, surveilActions, notifications) {
+                    $scope.sendDowntime = function () {
+                        angular.forEach($scope.tableId, function (tableId) {
+                            tables.forEachCheckedEntries(tableId, function (entry) {
+                                var service_description;
+
+                                if ('description' in entry) {
+                                    service_description = entry.description;
+                                }
+
+                                surveilActions.downtime(entry.host_name, service_description, $scope.attrs).then(function (data) {
+                                        notifications.push('success', 'Downtime', 'Added downtime for ' + entry.host_name);
+                                    },
+                                    function (error) {
+                                        notifications.push('error', 'Downtime', 'Could not add downtime for ' + entry.host_name);
+                                    });
+                            });
+
+                            tables.setAllCheckTable(tableId, false);
+                        });
+
+                        $scope.isShown = false;
+                    };
+                }
+            ]
         };
     })
-
-    .controller('banshoDowntimeFormCtrl',
-        ['$scope', '$filter', 'tablesConfig', 'actionbarFilters', 'surveilActions', 'notifications',
-        function ($scope, $filter, tablesConfig, actionbarFilters, surveilActions, notifications) {
-
-        $scope.sendDowntime = function () {
-            angular.forEach(tablesConfig, function (table) {
-                var entries = $filter('filter')(table.entries, actionbarFilters.searchFilter);
-                table.isCheckAll = false;
-
-                angular.forEach(entries, function (entry) {
-                    var service_description;
-
-                    if (entry.is_checked) {
-                        entry.is_checked = false;
-                        if ('description' in entry) {
-                            service_description = entry.description;
-                        }
-
-                        surveilActions.downtime(entry.host_name, service_description, $scope.attrs).then(function (data) {
-                            notifications.push('success', 'Downtime', 'Added downtime for ' + entry.host_name);
-                        },
-                        function (error) {
-                            notifications.push('error', 'Downtime', 'Could not add downtime for ' + entry.host_name);
-                        });
-                    }
-                });
-            });
-            $scope.isShown = false;
-        };
-    }])
 
     .directive('banshoRecheckButton', function () {
         return {
-            restrict: 'E',
-            templateUrl: 'components/table/actionbar/actions/recheck_button.html',
-            controller: 'banshoRecheckButtonCtrl'
-        };
-    })
+                restrict: 'E',
+                templateUrl: 'components/table/actionbar/actions/recheck_button.html',
+                controller: ['$scope', 'tables', 'surveilActions', 'notifications',
+                    function ($scope, tables, surveilActions, notifications) {
+                        $scope.sendRecheck = function () {
+                            angular.forEach($scope.tableId, function (tableId) {
+                                tables.forEachCheckedEntries(tableId, function (entry) {
+                                    var service_description;
 
-    .controller('banshoRecheckButtonCtrl',
-        ['$scope', '$filter', 'tablesConfig', 'actionbarFilters', 'surveilActions', 'notifications',
-        function ($scope, $filter, tablesConfig, actionbarFilters, surveilActions, notifications) {
+                                    if ('description' in entry) {
+                                        service_description = entry.description;
+                                    }
 
-        $scope.sendRecheck = function () {
-            angular.forEach(tablesConfig, function (table) {
-                var entries = $filter('filter')(table.entries, actionbarFilters.searchFilter);
-                table.isCheckAll = false;
+                                    surveilActions.recheck(entry.host_name, service_description).then(function (data) {
+                                            notifications.push('success', 'Recheck', 'Scheduled recheck for ' + entry.host_name);
+                                        },
+                                        function (error) {
+                                            notifications.push('error', 'Recheck', 'Could not schedule recheck for ' + entry.host_name);
+                                        });
+                                });
 
-                angular.forEach(entries, function (entry) {
-                    var service_description;
-
-                    if (entry.is_checked) {
-                        entry.is_checked = false;
-                        if ('description' in entry) {
-                            service_description = entry.description;
-                        }
-
-                        surveilActions.recheck(entry.host_name, service_description).then(function (data) {
-                            notifications.push('success', 'Recheck', 'Scheduled recheck for ' + entry.host_name);
-                        },
-                        function (error) {
-                            notifications.push('error', 'Recheck', 'Could not schedule recheck for ' + entry.host_name);
-                        });
+                                tables.setAllCheckTable(tableId, false);
+                            });
+                        };
                     }
-                });
-            });
-        };
-    }]);
+                ]
+            };
+        });
