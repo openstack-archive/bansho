8d5139b4355ced553ea1f947cea6ef71cc491c9d
Vincent Fournier <v.fournier11@gmail.com>
2015-08-24 10:32:29 -0400

    Add HTML5 notifications
    Change-Id: I086ce9d7118e54e56bdac035925054fd627c337d

--- app/components/notifications/notifications.js
+++ app/components/notifications/notifications.js
@@ -1,11 +1,15 @@
-/*global PNotify */
+/*global PNotify, window, Notification */
 
 'use strict';
 
 angular.module('bansho.notifications', [])
     .service('notifications', [
         function () {
-            var push = function (type, title, message) {
+            var sendNotification = function (type, title, message) {
+                new Notification(title, {body: message});
+            };
+
+            var sendPNotify = function (type, title, message) {
                 $(function(){
                     new PNotify({
                         type: type,
@@ -14,6 +18,24 @@ angular.module('bansho.notifications', [])
                         styling: {},
                     });
                 });
+            };
+
+            var push = function (type, title, message) {
+                if (window.Notification && Notification.permission === "granted") {
+                    sendNotification(type, title, message);
+                } else if (window.Notification && Notification.permission !== "denied") {
+                    Notification.requestPermission(function (status) {
+                        if (Notification.permission !== status) {
+                            Notification.permission = status;
+                        }
+
+                        if (status === "granted") {
+                            sendNotification(type, title, message);
+                        } else {
+                            sendPNotify(type, title, message);
+                        }
+                    });
+                }
             };
 
             return {
